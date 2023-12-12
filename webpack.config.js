973dd8e8618b70020bb2f1bdaa593bfaac013e4c
PavelSteep <bpv53@ukr.net>
2023-12-12 23:17:38 +0100

    change 8

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   output: {
-    path:path.join(__dirname, '/dist'),
+    path:path.join(__dirname, '/build'),
     filename:'[name].[contenthash].js'
   },
   devServer:{
